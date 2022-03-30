76ae12dc3a8e3f90003c70b1426699f49f5f34d4
Ben <robinsonbena@gmail.com>
2022-03-30 16:17:06 -0700

    initial commit, all tests passed except inject

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,85 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    if block_given?
+      i = 0
+      until i == self.length do
+        yield(self[i], i)
+        i += 1
+      end
+    end
+    self
+  end
+
+  def my_select
+    if block_given?
+      new_array = []
+      my_each { |i| new_array << i if yield(i)}
+      new_array
+    else
+      self
+    end
+  end
+
+  def my_all?
+    if block_given?
+      result = true
+      my_each { |i| result = false if yield(i) == false }
+      result
+    else
+      self
+    end
+  end
+
+  def my_any?
+    if block_given?
+      result = false
+      my_each { |i| result = true if yield(i) }
+      result
+    else
+      self
+    end
+  end
+
+  def my_none?
+    if block_given?
+      result = true
+      my_each { |i| result = false if yield(i) }
+      result
+    else
+      self
+    end
+  end
+
+  def my_count
+    if block_given?
+      counter = 0
+      my_each { |i| counter += 1 if yield(i) }
+      counter
+    else
+      self
+    end
+  end
+
+  def my_map
+    if block_given?
+      map_array = []
+      my_each { |i| map_array << yield(i) }
+      map_array
+    else
+      self
+    end
+  end
+
+  def inject(accumulator)
+    if block_given?
+      base = accumulator
+      my_each { |i| base = yield(i, base) }
+      base
+    else
+      self
+    end
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +87,16 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  
+  def my_each
+    if block_given?
+      i = 0
+      until i == self.length do
+        yield(self[i])
+        i += 1
+      end
+    end
+    self
+  end
+
 end
